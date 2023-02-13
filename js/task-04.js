d2bb8865b54f80bf959b9844fa300a97f485c835
Denys-Kondratenko <dk132652@gmail.com>
2023-02-13 16:32:08 +0200

    виконав шість завдань

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
+
+btnDecrement.addEventListener('click', () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
+
+btnIncrement.addEventListener('click', () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
