a6d8cf250f4d48343b29ef0024f4518617fe9852
Denys-Kondratenko <dk132652@gmail.com>
2023-02-14 17:34:33 +0200

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,11 @@ const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 
 inputName.addEventListener('input', event => {
-  outputName.textContent = event.currentTarget.value;
+  event.currentTarget.value = event.currentTarget.value.trim();
+
+  if (event.currentTarget.value === '') {
+    outputName.textContent = 'Anonymous';
+  } else {
+    outputName.textContent = event.currentTarget.value;
+  }
 });
