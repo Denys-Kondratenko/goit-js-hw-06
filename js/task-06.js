2d218be090e2c83e1fbf3574ce765769ff7ea51a
Denys-Kondratenko <dk132652@gmail.com>
2023-02-14 17:27:43 +0200

    update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,8 @@
 const inputValidation = document.querySelector('#validation-input');
 
 inputValidation.addEventListener('blur', () => {
+  inputValidation.value = inputValidation.value.trim();
+
   if (inputValidation.value.length === Number(inputValidation.dataset.length)) {
     inputValidation.classList.add('valid');
     inputValidation.classList.remove('invalid');
